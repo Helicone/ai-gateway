087113492371dc3939b45f0d90508ff90cd7969c
Kavin Valli <mail@kavin.me>
2025-07-11 03:26:33 -0700

    feat: authenticate /ai with key already loaded in db

--- ai-gateway/src/middleware/auth.rs
+++ ai-gateway/src/middleware/auth.rs
@@ -17,6 +17,12 @@ pub struct AuthService {
     app_state: AppState,
 }
 
+const UNIFIED_URL_REGEX: &str =
+    r"^/(?P<first_segment>[^/?]+)(?P<rest>/[^?]*)?(?P<query>\?.*)?$";
+
+const ROUTER_URL_REGEX: &str =
+    r"^/router/(?P<id>[A-Za-z0-9_-]{1,12})(?P<path>/[^?]*)?(?P<query>\?.*)?$";
+
 impl AuthService {
     #[must_use]
     pub fn new(app_state: AppState) -> Self {
@@ -34,35 +40,119 @@ impl AuthService {
 
         match app_state.0.config.deployment_target {
             DeploymentTarget::Cloud => {
-                let regex = Regex::new(ROUTER_URL_REGEX).unwrap();
-                let captures = regex.captures(path);
-                if captures.is_none() {
-                    return Err(AuthError::MissingRouterId);
-                }
-                let id_str = captures
-                    .unwrap()
-                    .name("id")
-                    .ok_or_else(|| AuthError::MissingRouterId)?
-                    .as_str();
-                let router_id = RouterId::Named(id_str.into());
-                if let Some(router_api_keys) =
-                    app_state.get_router_api_keys().await
-                    && router_api_keys.contains_key(&router_id)
+                // let allowed_keys = if let Some(captures) =
+                //     Regex::new(UNIFIED_URL_REGEX).unwrap().captures(path)
+                // {
+                //     let first_segment = captures
+                //         .name("first_segment")
+                //         .ok_or_else(|| AuthError::MissingRouterId)?
+                //         .as_str();
+
+                //     if let Some(router_api_keys) =
+                //         app_state.get_router_api_keys().await
+                //     {
+                //         if first_segment == "router"
+                //             && router_api_keys.contains_key(&router_id)
+                //         {
+                //             router_api_keys.get(&router_id).unwrap()
+                //         } else if first_segment == "ai"
+                //             && router_api_keys.contains_key(&RouterId::Named(
+                //                 "hcone_rsv_ai".into(),
+                //             ))
+                //         {
+                //             router_api_keys
+                //                 .get(&RouterId::Named("hcone_rsv_ai".into()))
+                //                 .unwrap()
+                //         } else {
+                //             return Err(AuthError::InvalidCredentials);
+                //         }
+                //     }
+                // };
+
+                if let Some(captures) =
+                    Regex::new(UNIFIED_URL_REGEX).unwrap().captures(path)
                 {
-                    let allowed_keys = router_api_keys.get(&router_id).unwrap();
-                    let key = allowed_keys
-                        .iter()
-                        .find(|k| k.key_hash == computed_hash);
-                    if let Some(key) = key {
-                        return Ok(AuthContext {
-                            api_key: Secret::from(api_key_without_bearer),
-                            user_id: key.owner_id.as_str().try_into()?,
-                            org_id: config
-                                .auth
-                                .organization_id
-                                .as_str()
-                                .try_into()?,
-                        });
+                    let first_segment = captures
+                        .name("first_segment")
+                        .ok_or_else(|| AuthError::MissingRouterId)?
+                        .as_str();
+
+                    if first_segment == "router" {
+                        let regex = Regex::new(ROUTER_URL_REGEX).unwrap();
+                        let captures = regex.captures(path);
+                        if captures.is_none() {
+                            return Err(AuthError::MissingRouterId);
+                        }
+                        let id_str = captures
+                            .unwrap()
+                            .name("id")
+                            .ok_or_else(|| AuthError::MissingRouterId)?
+                            .as_str();
+                        let router_id = RouterId::Named(id_str.into());
+                        if let Some(router_api_keys) =
+                            app_state.get_router_api_keys().await
+                            && router_api_keys.contains_key(&router_id)
+                        {
+                            let allowed_keys =
+                                router_api_keys.get(&router_id).unwrap();
+                            let key = allowed_keys
+                                .iter()
+                                .find(|k| k.key_hash == computed_hash);
+                            if let Some(key) = key {
+                                return Ok(AuthContext {
+                                    api_key: Secret::from(
+                                        api_key_without_bearer,
+                                    ),
+                                    user_id: key
+                                        .owner_id
+                                        .as_str()
+                                        .try_into()?,
+                                    org_id: config
+                                        .auth
+                                        .organization_id
+                                        .as_str()
+                                        .try_into()?,
+                                });
+                            } else {
+                                Err(AuthError::InvalidCredentials)
+                            }
+                        } else {
+                            Err(AuthError::InvalidCredentials)
+                        }
+                    } else if first_segment == "ai" {
+                        if let Some(router_api_keys) =
+                            app_state.get_router_api_keys().await
+                            && router_api_keys.contains_key(&RouterId::Named(
+                                "hcone_rsv_ai".into(),
+                            ))
+                        {
+                            let allowed_keys = router_api_keys
+                                .get(&RouterId::Named("hcone_rsv_ai".into()))
+                                .unwrap();
+                            let key = allowed_keys
+                                .iter()
+                                .find(|k| k.key_hash == computed_hash);
+                            if let Some(key) = key {
+                                return Ok(AuthContext {
+                                    api_key: Secret::from(
+                                        api_key_without_bearer,
+                                    ),
+                                    user_id: key
+                                        .owner_id
+                                        .as_str()
+                                        .try_into()?,
+                                    org_id: config
+                                        .auth
+                                        .organization_id
+                                        .as_str()
+                                        .try_into()?,
+                                });
+                            } else {
+                                Err(AuthError::InvalidCredentials)
+                            }
+                        } else {
+                            Err(AuthError::InvalidCredentials)
+                        }
                     } else {
                         Err(AuthError::InvalidCredentials)
                     }
@@ -89,9 +179,6 @@ impl AuthService {
         }
     }
 }
-
-const ROUTER_URL_REGEX: &str =
-    r"^/router/(?P<id>[A-Za-z0-9_-]{1,12})(?P<path>/[^?]*)?(?P<query>\?.*)?$";
 
 impl<B> AsyncAuthorizeRequest<B> for AuthService
 where
--- ai-gateway/src/store/router_store.rs
+++ ai-gateway/src/store/router_store.rs
@@ -26,6 +26,12 @@ pub struct DBRouterKeys {
     pub user_id: Uuid,
 }
 
+#[derive(Debug, sqlx::FromRow)]
+pub struct DBUnifiedApiKeys {
+    pub api_key_hash: String,
+    pub user_id: Uuid,
+}
+
 impl RouterStore {
     pub fn new(pool: PgPool) -> Result<Self, InitError> {
         Ok(Self { pool })
@@ -57,7 +63,7 @@ impl RouterStore {
              helicone_api_keys.user_id FROM router_keys INNER JOIN \
              helicone_api_keys ON router_keys.api_key_id = \
              helicone_api_keys.id INNER JOIN routers ON router_keys.router_id \
-             = routers.id",
+             = routers.id WHERE routers.hash IS NOT NULL",
         )
         .fetch_all(&self.pool)
         .await
@@ -76,6 +82,30 @@ impl RouterStore {
                     owner_id: r.user_id.to_string(),
                 });
         }
+
+        // get /ai keys - router_keys.router_id is NULL
+        let res = sqlx::query_as::<_, DBUnifiedApiKeys>(
+            "SELECT helicone_api_keys.api_key_hash, \
+             helicone_api_keys.user_id FROM router_keys INNER JOIN \
+             helicone_api_keys ON router_keys.api_key_id = \
+             helicone_api_keys.id WHERE router_keys.router_id IS NULL",
+        )
+        .fetch_all(&self.pool)
+        .await
+        .map_err(|e| {
+            error!(error = %e, "failed to get all router keys");
+            InitError::DatabaseConnection(e)
+        })?;
+
+        for r in res {
+            map.entry(RouterId::Named("hcone_rsv_ai".into()))
+                .or_insert(vec![])
+                .push(Key {
+                    key_hash: r.api_key_hash,
+                    owner_id: r.user_id.to_string(),
+                });
+        }
+
         tracing::info!("map length: {:?}", map.len());
         Ok(map)
     }
