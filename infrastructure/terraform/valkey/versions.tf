028717d837d90446ff63f12281413a647b5d237c
Devin Ersoy <devin@helicone.ai>
2025-07-22 11:49:32 -0700

    feat: valkey terraform resources

---
+++ infrastructure/terraform/valkey/versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  cloud { 
+    organization = "helicone" 
+    workspaces { 
+      name = "helicone-valkey" 
+    } 
+  }
+  required_version = ">= 1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.80"
+    }
+  }
+} 
